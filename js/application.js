7e8a1635b3dcaa0470b9c6d6577fa56bf1389f5a
Adam Wentz <awentz@theonion.com>
2014-03-21 14:53:51 -0500

    ADVANCED GAME-SOLVING AI

--- js/application.js
+++ js/application.js
@@ -1,4 +1,14 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+var gm = null;
 window.requestAnimationFrame(function () {
-  new GameManager(16, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  gm = new GameManager(16, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  window.requestAnimationFrame(autoMoves);
 });
+
+
+function autoMoves() {
+    if (gm === null)
+        return;
+    gm.inputManager.emit("move", parseInt(Math.random() * 4));
+    window.requestAnimationFrame(autoMoves);
+}
